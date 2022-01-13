18433731464f3559b2f4895bd79c9e2d146b4f98
Luccas Marcos <luccasmarcos002@gmail.com>
2022-01-13 09:43:27 -0300

    Project created

---
+++ src/main/java/com/luccascyber/workshopspringboot2jpa/WorkshopSpringboot2JpaApplication.java
@@ -0,0 +1,12 @@
+package com.luccascyber.workshopspringboot2jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringboot2JpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringboot2JpaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/luccascyber/workshopspringboot2jpa/WorkshopSpringboot2JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luccascyber.workshopspringboot2jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringboot2JpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
