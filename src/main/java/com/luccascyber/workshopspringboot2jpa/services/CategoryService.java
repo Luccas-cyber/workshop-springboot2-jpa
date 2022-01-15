4e568228ea919c8e562594ebb5b0a0c801735d39
Luccas Marcos <luccasmarcos002@gmail.com>
2022-01-15 12:03:03 -0300

    Category

---
+++ src/main/java/com/luccascyber/workshopspringboot2jpa/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.luccascyber.workshopspringboot2jpa.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.luccascyber.workshopspringboot2jpa.entities.Category;
+import com.luccascyber.workshopspringboot2jpa.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+}
