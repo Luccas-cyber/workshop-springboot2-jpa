40d85faa90b722b9f4c2d07f4366cfe9a7819988
Luccas Marcos <luccasmarcos002@gmail.com>
2022-01-19 10:57:03 -0300

    Exception handling - findById

---
+++ src/main/java/com/luccascyber/workshopspringboot2jpa/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.luccascyber.workshopspringboot2jpa.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found. Id " + id);
+	}
+}
